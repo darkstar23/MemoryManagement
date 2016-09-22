e017c1bae04ec77f6a57362f4469de854b844eb7
darkstar23 <mbcaracta@mymail.mapua.edu.ph>
2016-09-22 18:49:06 +0800

    Error handling somehow fixed, added procedural adding of jobs, confirmation of jobs added, and some bug fixes.

--- MemoryManagement/Form1.Designer.cs
+++ MemoryManagement/Form1.Designer.cs
@@ -31,19 +31,28 @@
             this.label_PID = new System.Windows.Forms.Label();
             this.label_ProcSize = new System.Windows.Forms.Label();
             this.label_ProcDuration = new System.Windows.Forms.Label();
-            this.textBox_PID = new System.Windows.Forms.TextBox();
+            this.textBox_NUM_JOBS = new System.Windows.Forms.TextBox();
             this.textBox_ProcSize = new System.Windows.Forms.TextBox();
             this.textBox_ProcDuration = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_PID_NUM = new System.Windows.Forms.Label();
+            this.textBox_CompactionT = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_CoalescingT = new System.Windows.Forms.TextBox();
+            this.button_Add = new System.Windows.Forms.Button();
+            this.button_NEXT_JOB = new System.Windows.Forms.Button();
+            this.richTextBox_Updater = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // label_PID
             // 
             this.label_PID.AutoSize = true;
-            this.label_PID.Location = new System.Drawing.Point(12, 56);
+            this.label_PID.Location = new System.Drawing.Point(12, 66);
             this.label_PID.Name = "label_PID";
-            this.label_PID.Size = new System.Drawing.Size(28, 13);
+            this.label_PID.Size = new System.Drawing.Size(81, 13);
             this.label_PID.TabIndex = 0;
-            this.label_PID.Text = "PID:";
+            this.label_PID.Text = "Number of jobs:";
             this.label_PID.Click += new System.EventHandler(this.label1_Click);
             // 
             // label_ProcSize
@@ -51,48 +60,136 @@
             this.label_ProcSize.AutoSize = true;
             this.label_ProcSize.Location = new System.Drawing.Point(12, 92);
             this.label_ProcSize.Name = "label_ProcSize";
-            this.label_ProcSize.Size = new System.Drawing.Size(71, 13);
+            this.label_ProcSize.Size = new System.Drawing.Size(94, 13);
             this.label_ProcSize.TabIndex = 1;
-            this.label_ProcSize.Text = "Process Size:";
+            this.label_ProcSize.Text = "Process Size (KB):";
             // 
             // label_ProcDuration
             // 
             this.label_ProcDuration.AutoSize = true;
-            this.label_ProcDuration.Location = new System.Drawing.Point(12, 138);
+            this.label_ProcDuration.Location = new System.Drawing.Point(12, 118);
             this.label_ProcDuration.Name = "label_ProcDuration";
-            this.label_ProcDuration.Size = new System.Drawing.Size(91, 13);
+            this.label_ProcDuration.Size = new System.Drawing.Size(115, 13);
             this.label_ProcDuration.TabIndex = 2;
-            this.label_ProcDuration.Text = "Process Duration:";
+            this.label_ProcDuration.Text = "Process Duration (TU):";
             // 
-            // textBox_PID
+            // textBox_NUM_JOBS
             // 
-            this.textBox_PID.Location = new System.Drawing.Point(111, 53);
-            this.textBox_PID.Name = "textBox_PID";
-            this.textBox_PID.Size = new System.Drawing.Size(81, 20);
-            this.textBox_PID.TabIndex = 3;
+            this.textBox_NUM_JOBS.Location = new System.Drawing.Point(133, 63);
+            this.textBox_NUM_JOBS.Name = "textBox_NUM_JOBS";
+            this.textBox_NUM_JOBS.Size = new System.Drawing.Size(81, 20);
+            this.textBox_NUM_JOBS.TabIndex = 3;
             // 
             // textBox_ProcSize
             // 
-            this.textBox_ProcSize.Location = new System.Drawing.Point(111, 89);
+            this.textBox_ProcSize.Location = new System.Drawing.Point(133, 89);
             this.textBox_ProcSize.Name = "textBox_ProcSize";
             this.textBox_ProcSize.Size = new System.Drawing.Size(81, 20);
             this.textBox_ProcSize.TabIndex = 4;
             // 
             // textBox_ProcDuration
             // 
-            this.textBox_ProcDuration.Location = new System.Drawing.Point(111, 135);
+            this.textBox_ProcDuration.Location = new System.Drawing.Point(133, 115);
             this.textBox_ProcDuration.Name = "textBox_ProcDuration";
             this.textBox_ProcDuration.Size = new System.Drawing.Size(81, 20);
             this.textBox_ProcDuration.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Compaction time:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "PID:";
+            // 
+            // label_PID_NUM
+            // 
+            this.label_PID_NUM.AutoSize = true;
+            this.label_PID_NUM.Location = new System.Drawing.Point(159, 47);
+            this.label_PID_NUM.Name = "label_PID_NUM";
+            this.label_PID_NUM.Size = new System.Drawing.Size(25, 13);
+            this.label_PID_NUM.TabIndex = 8;
+            this.label_PID_NUM.Text = "PID";
+            // 
+            // textBox_CompactionT
+            // 
+            this.textBox_CompactionT.Location = new System.Drawing.Point(133, 141);
+            this.textBox_CompactionT.Name = "textBox_CompactionT";
+            this.textBox_CompactionT.Size = new System.Drawing.Size(81, 20);
+            this.textBox_CompactionT.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Coalescing time:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox_CoalescingT
+            // 
+            this.textBox_CoalescingT.Location = new System.Drawing.Point(133, 167);
+            this.textBox_CoalescingT.Name = "textBox_CoalescingT";
+            this.textBox_CoalescingT.Size = new System.Drawing.Size(81, 20);
+            this.textBox_CoalescingT.TabIndex = 11;
+            // 
+            // button_Add
+            // 
+            this.button_Add.Location = new System.Drawing.Point(231, 61);
+            this.button_Add.Name = "button_Add";
+            this.button_Add.Size = new System.Drawing.Size(127, 23);
+            this.button_Add.TabIndex = 12;
+            this.button_Add.Text = "ADD JOBS";
+            this.button_Add.UseVisualStyleBackColor = true;
+            this.button_Add.Click += new System.EventHandler(this.button_Add_Click);
+            // 
+            // button_NEXT_JOB
+            // 
+            this.button_NEXT_JOB.Location = new System.Drawing.Point(231, 92);
+            this.button_NEXT_JOB.Name = "button_NEXT_JOB";
+            this.button_NEXT_JOB.Size = new System.Drawing.Size(127, 23);
+            this.button_NEXT_JOB.TabIndex = 13;
+            this.button_NEXT_JOB.Text = "NEXT JOB";
+            this.button_NEXT_JOB.UseVisualStyleBackColor = true;
+            this.button_NEXT_JOB.Click += new System.EventHandler(this.button_NEXT_JOB_Click);
+            // 
+            // richTextBox_Updater
+            // 
+            this.richTextBox_Updater.Location = new System.Drawing.Point(393, 47);
+            this.richTextBox_Updater.Name = "richTextBox_Updater";
+            this.richTextBox_Updater.Size = new System.Drawing.Size(262, 96);
+            this.richTextBox_Updater.TabIndex = 14;
+            this.richTextBox_Updater.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(535, 426);
+            this.ClientSize = new System.Drawing.Size(667, 266);
+            this.Controls.Add(this.richTextBox_Updater);
+            this.Controls.Add(this.button_NEXT_JOB);
+            this.Controls.Add(this.button_Add);
+            this.Controls.Add(this.textBox_CoalescingT);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_CompactionT);
+            this.Controls.Add(this.label_PID_NUM);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox_ProcDuration);
             this.Controls.Add(this.textBox_ProcSize);
-            this.Controls.Add(this.textBox_PID);
+            this.Controls.Add(this.textBox_NUM_JOBS);
             this.Controls.Add(this.label_ProcDuration);
             this.Controls.Add(this.label_ProcSize);
             this.Controls.Add(this.label_PID);
@@ -109,9 +206,18 @@
         private System.Windows.Forms.Label label_PID;
         private System.Windows.Forms.Label label_ProcSize;
         private System.Windows.Forms.Label label_ProcDuration;
-        private System.Windows.Forms.TextBox textBox_PID;
+        private System.Windows.Forms.TextBox textBox_NUM_JOBS;
         private System.Windows.Forms.TextBox textBox_ProcSize;
         private System.Windows.Forms.TextBox textBox_ProcDuration;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_PID_NUM;
+        private System.Windows.Forms.TextBox textBox_CompactionT;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_CoalescingT;
+        private System.Windows.Forms.Button button_Add;
+        private System.Windows.Forms.Button button_NEXT_JOB;
+        private System.Windows.Forms.RichTextBox richTextBox_Updater;
     }
 }
 
--- MemoryManagement/Form1.cs
+++ MemoryManagement/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace MemoryManagement
 {
@@ -20,12 +21,20 @@ namespace MemoryManagement
        public List<Process> ALL_PROC_LIST = new List<Process>();
        public List<Process> ACTIVE_PROC_LIST = new List<Process>();
        public List<Process> SUSPENDED_PROC_LIST = new List<Process>();
+        int CNT_TO_NEXT_JOB = 0;
 
-       public enum ProcessStatus { RUNNING, WAITING, FINISHED };
+        public enum ProcessStatus { RUNNING, WAITING, FINISHED };
 
        public Form1()
        {
            InitializeComponent();
+            textBox_CoalescingT.Enabled = false;
+            textBox_CompactionT.Enabled = false;
+            textBox_ProcSize.Enabled = false;
+            textBox_ProcDuration.Enabled = false;
+            button_NEXT_JOB.Enabled = false;
+            
+
        }
 
        private void Form1_Load(object sender, EventArgs e)
@@ -37,5 +46,56 @@ namespace MemoryManagement
        {
 
        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Add_Click(object sender, EventArgs e)
+        {
+            if(textBox_NUM_JOBS != null && Regex.IsMatch(textBox_NUM_JOBS.Text, @"^\d+$"))
+            {
+                textBox_NUM_JOBS.Enabled = false;
+                button_NEXT_JOB.Enabled = true;
+                textBox_ProcDuration.Enabled = true;
+                textBox_ProcSize.Enabled = true;
+                button_Add.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Please input a valid number.");
+                textBox_NUM_JOBS.ResetText();
+            }
+        }
+
+        private void button_NEXT_JOB_Click(object sender, EventArgs e)
+        {
+            if(Regex.IsMatch(textBox_ProcDuration.Text, @"^\d+$") && Regex.IsMatch(textBox_ProcSize.Text, @"^\d+$") && CNT_TO_NEXT_JOB < int.Parse(textBox_NUM_JOBS.Text) && int.Parse(textBox_ProcSize.Text) % 50 == 0)
+            {
+                ALL_PROC_LIST.Add(new MemoryManagement.Process((CNT_TO_NEXT_JOB + 1), int.Parse(textBox_ProcSize.Text), int.Parse(textBox_ProcDuration.Text), ProcessStatus.WAITING));
+                label_PID_NUM.Text = (CNT_TO_NEXT_JOB + 1).ToString();
+                richTextBox_Updater.AppendText("PID: "+ (CNT_TO_NEXT_JOB + 1).ToString() + " | Size:" + textBox_ProcSize.Text + " | Duration: " + textBox_ProcDuration.Text + "\n") ;
+                textBox_ProcDuration.ResetText();
+                textBox_ProcSize.ResetText();
+                CNT_TO_NEXT_JOB += 1;
+
+                
+            }
+            else if(CNT_TO_NEXT_JOB == int.Parse(textBox_NUM_JOBS.Text))
+            {
+                MessageBox.Show("Max Jobs are now at state waiting!");
+                textBox_ProcDuration.Enabled = false;
+                textBox_ProcSize.Enabled = false;
+                button_NEXT_JOB.Enabled = false;
+                textBox_CoalescingT.Enabled = true;
+                textBox_CompactionT.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number, or a number divisible by 50. you should feel bad about yourself, not going to reset that field so you'll realize your mistake.");
+            }
+                
+        }
     }
 }
